bacf7401415912ad0d79ca9cde3091d2c8764e7f
jimmy_home <jimmy@motherapp.com>
2016-10-22 20:12:36 -0400

    write tokenize

--- tests/tokenize.ml
+++ tests/tokenize.ml
@@ -1,65 +1,49 @@
 open Parser
 open Ast
 
-type num =
-  | Num_int of int
-  | Num_float of float 
-
 let stringify = function
-  (* Punctuation *)
-  | LPAREN -> "LPAREN"  | RPAREN -> "RPAREN"
-  | LCAR -> "LCAR"      | RCAR -> "RCAR"
-  | LBRACE -> "LBRACE"  | RBRACE -> "RBRACE"
-  | COMMA -> "COMMA"    | VBAR -> "VBAR"
-  | DDELIM -> "DDELIM"  | DISC -> "DISC"
-
-  (* Dist Operators *)
-  | DPLUS -> "DPLUS"    | DTIMES -> "DTIMES"
-  | DPOWER -> "DPOWER"  | DSHIFT -> "DSHIFT"
-  | DSTRETCH -> "DSTRETCH"
-
-  (* Arithmetic Operators *)
-  | PLUS -> "PLUS"     | MINUS -> "MINUS"
-  | TIMES -> "TIMES"   | DIVIDE -> "DIVIDE"
-  | MOD -> "MOD"       | POWER -> "POWER" 
-
-  (* Relational Operators *)
-  | EQ -> "EQ"    | NEQ -> "NEQ"
-  | LEQ -> "LEQ"  | GEQ -> "GEQ"
-
-  (* List Operators *)
-  | CONS -> "CONS"
-
-  (* Logical Operators & Keywords *)
-  | AND -> "AND"   | OR -> "OR"
-  | NOT -> "NOT"
-
-  (* Assignment Operator *)
-  | ASN -> "ASN"
-
-  (* Conditional Operators *)
-  | IF -> "IF"    | THEN -> "THEN"
-  | ELSE -> "ELSE"
-
-  (* Declarative Keywords *)
-  | DO -> "DO"
-
-  (* Function Symbols & Keywords *)
-  | FDELIM -> "FDELIM"
-  | RETURN -> "RETURN"
-  | CAKE -> "CAKE"
-
+  (* calculation *)
+  | PLUS -> "PLUS"   | MINUS -> "MINUS"
+  | TIMES -> "TIMES" | DIVIDE -> "DIVIDE"
+  | MOD -> "MOD"
+  (* separator *)
+  | SEMICOLUMN -> "SEMICOLUMN" | SEQUENCE -> "SEQUENCE"
+  | ASSIGN -> "ASSIGN"         | COLUMN -> "COLUMN"
+  | DOT -> "DOT"
+  (* logical operation *)
+  | AND -> "AND"      | OR -> "OR"
+  | NOT -> "NOT"      | IF -> "IF"
+  | ELSE -> "ELSE"    | FOR -> "FOR"
+  | BREAK -> "BREAK"  | CONTINUE -> "CONTINUE"
+  | IN -> "IN"  
+  (* comparator *)
+  | GREATER -> "GREATER"      | GREATEREQUAL -> "GREATEREQUAL"
+  | SMALLER -> "SMALLER"      | SMALLEREQUAL -> "SMALLEREQUAL"
+  (* graph operator *)
+  | LINK -> "LINK"            | RIGHTLINK -> "RIGHTLINK"
+  | LEFTLINK -> "LEFTLINK"    
+  (* identifier *)
+  | ID(string) -> "ID"
+  (* primary type *)
+  | INT -> "INT"          | FLOAT -> "FLOAT"
+  | STRING -> "STRING"    | BOOL -> "BOOL"
+  | NODE -> "NODE"        | GRAPH -> "GRAPH"
+  | LIST -> "LIST"        | DICT -> "DICT"
+  | NULL -> "NULL"
+  (* quote *)
+  | QUOTE -> "QUOTE"
+  (* boolean operation *)
+  | TRUE -> "TRUE" | FALSE -> "FALSE" 
+  (* bracket *)
+  | LEFTBRACKET -> "LEFTBRACKET"           | RIGHTBRACKET -> "RIGHTBRACKET" 
+  | LEFTCURLYBRACKET -> "LEFTCURLYBRACKET" | RIGHTCURLYBRACKET -> "RIGHTCURLYBRACKET" 
+  | LEFTROUNDBRACKET -> "LEFTROUNDBRACKET" | RIGHTROUNDBRACKET -> "RIGHTROUNDBRACKET" 
   (* End-of-File *)
   | EOF -> "EOF"
-
-  (* Identifiers *)
-  | ID(string) -> "ID"
-
   (* Literals *)
-  | NUM_LITERAL(num) -> "NUM_LITERAL"
+  | INT_LITERAL(int) -> "INT_LITERAL"
+  | FLOAT_LITERAL(float) -> "FLOAT_LITERAL"
   | STRING_LITERAL(string) -> "STRING_LITERAL"
-  | BOOL_LITERAL(bool) -> "BOOL_LITERAL"
-  | VOID_LITERAL -> "VOID_LITERAL"
 
 let _ = 
   let lexbuf = Lexing.from_channel stdin in
