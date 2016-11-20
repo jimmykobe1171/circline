f61cc76f47b604b5a002687375af3c0a410e7f7a
lanking520 <lanking520@gmail.com>
2016-11-19 22:49:09 -0500

    Build pig.sh

--- compiler/pig.sh
+++ compiler/pig.sh
@@ -1,6 +1,12 @@
 make clean
 ocamlc -c ast.ml
 ocamlc -c cast.ml
+ocamlyacc parser.mly
+ocamllex scanner.mll
+ocamlc -c parser.mli # compile parser types
+ocamlc -c scanner.ml
+ocamlc -c parser.ml
 ocamlc -c organizer.ml
-ocamlc -o pig ast.cmo cast.cmo organizer.cmo
+ocamlc -c parserize_cast.ml
+ocamlc -o pig ast.cmo cast.cmo parser.cmo scanner.cmo organizer.cmo parserize_cast.cmo
 ./pig
