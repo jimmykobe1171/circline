38f9ad49f4f4673dab4476a29067068acfa848bc
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-19 19:16:25 -0500

    Renew cast

--- compiler/cast.ml
+++ compiler/cast.ml
@@ -20,7 +20,7 @@ type binop =
 
 (* Unary Operators *)
 type unop =
-  Sub         (* - *)
+  Neg         (* - *)
 | Not         (* not *)
 
 (* Numbers int | float *)
@@ -36,12 +36,16 @@ type var_type =
 | Bool_t
 | Node_t
 | Graph_t
-| List_t
 | Dict_Int_t
 | Dict_Float_t
 | Dict_String_t
 | Dict_Node_t
 | Dict_Graph_t
+| List_Int_t
+| List_Float_t
+| List_String_t
+| List_Node_t
+| List_Graph_t
 | Void_t
 | Null_t
 
@@ -60,20 +64,24 @@ type expr =
 |   String_Lit of string
 |   Bool_lit of bool
 |   Node of expr
-| 	Graph_Link of expr * graph_op * expr * expr
-| 	Binop of expr * binop * expr
-|  	Unop of unop * expr
+|   Graph_Link of expr * graph_op * expr * expr
+|   Binop of expr * binop * expr
+|   Unop of unop * expr
 |   Id of string
 |   Assign of string * expr
 |   Noexpr
 |   ListP of expr list
 |   DictP of (expr * expr) list
 |   Call of string * expr list    (* function call *)
+|   CallDefault of expr * string * expr list
 
 and edge_graph_list = {
   graphs: expr list;
   edges: expr list;
 }
+
+type var_decl =
+| Local of var_type * string * expr
 
 (* Statements *)
 type stmt =
