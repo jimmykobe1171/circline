9a8239d40a6489772aba16e615fe2d3518f21394
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-20 20:48:35 -0500

    Add auto test of code gen

---
+++ tests/test_code_gen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NC='\033[0m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+result=true
+
+INPUT_FILES="code_gen/*.in"
+printf "${CYAN}Running code_gen tests...\n${NC}"
+
+
+
+for input_file in $INPUT_FILES; do
+    output_file=${input_file/.in/.out}
+    ../compiler/circline.native < $input_file > pig
+    lli pig | cmp -s $output_file -
+    if [ "$?" -eq 0 ]; then
+       printf "%-65s ${GREEN}SUCCESS\n${NC}" "  - checking $input_file..."
+    else
+       printf "%-65s ${RED}ERROR\n${NC}" "  - checking $input_file..." 1>&2
+    	result=false
+    fi
+done
+
+
+if $result; then
+	exit 0
+else
+	exit 1
+fi
