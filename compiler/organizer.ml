c41d6266a420f171ac79ba7be8c1a25ecdd619c4
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-26 14:49:55 -0500

    Format and comment.

--- compiler/organizer.ml
+++ compiler/organizer.ml
@@ -82,15 +82,9 @@ and convert_dict_list = function
   | [x] -> [convert_dict x]
   | _ as l -> (List.map convert_dict l)
 
-(*s
-let convert_stmt = function
-    [] -> []
-  | A.Expr(a) -> C.Expr(convert_expr a)
+let convert_edge_graph_list = function
+  {A.graphs = g; A.edges = e} -> {C.graphs = convert_expr_list g; C.edges = convert_expr_list e}
 
-let rec convert curr_list = function
-   [] -> curr_list
-  | s::ss -> (convert_stmt s) :: (convert curr_list ss)
-*)
 let convert_formal = function
   | A.Formal(v, s) -> C.Formal(convert_var_type v, s) 
 
@@ -99,29 +93,13 @@ let convert_formal_list = function
   | [x] -> [convert_formal x]
   | _ as l -> (List.map convert_formal l)
 
+(* create a main funcition outside of the whole statement list *)
 let createMain stmts = A.Func({
     A.returnType = A.Int_t;
     A.name = "main";
     A.args = [];
     A.body = stmts;
   })
-
-
-(*
-let convertAST stmts =
-  let (functions, variables, scripts) =
-    let aux (fs, vars, others)= function
-      | A.Func(_) as f -> (f::fs, vars, others)
-      | A.Var_dec(A.Local(typ, name, v)) -> (
-          match v with
-          | A.Noexpr -> (fs, A.Var_dec(A.Local(typ, name, A.Noexpr))::vars, others)
-          | _ -> (fs, A.Var_dec(A.Local(typ, name, A.Noexpr))::vars, A.Expr(A.Assign(name, v))::others)
-        )
-      | _ as other -> (fs, vars, other::others)
-    in List.fold_left aux ([], [], []) stmts
-  in (List.rev functions), (List.rev variables), (List.rev scripts) *)
-
-(* let get_local_from_body = function *)
 
 module StringMap = Map.Make(String)
 
@@ -135,12 +113,12 @@ let rec get_body_from_body_a = function
   | A.Func(_)::tl -> get_body_from_body_a tl
   | _ as x::tl -> x :: (get_body_from_body_a tl)
 
+(* convert functions in A to a horizental function list in A *)
 let rec convert_func_list m parent = function
     [] -> ([], m)
   | A.Func{A.returnType = r; A.name = n; A.args = a; A.body = b}::tl -> 
     let m = StringMap.add n parent m in 
-(*    print_int (StringMap.cardinal m); 
-    print_string " is the size!\n"; *)
+
     let addedFunc = A.Func({
       A.returnType = r; A.name = n; A.args = a; A.body = get_body_from_body_a b
     }) in 
@@ -149,7 +127,7 @@ let rec convert_func_list m parent = function
     ((addedFunc :: (fst firstRes) @ (fst secondRes)), (snd secondRes))
   | _::tl -> convert_func_list m parent tl
 
-
+(* convert stament in A to C, except those Var_dec and Func, we will convert them separately *)
 let rec convert_stmt = function
     A.Expr(a) -> C.Expr(convert_expr a)
   | A.Return(a) -> C.Return(convert_expr a)
@@ -159,24 +137,17 @@ let rec convert_stmt = function
   | A.Block(stls) ->C.Block(List.map convert_stmt stls)
   | _ -> C.Expr(C.Noexpr)
 
-(*
-let convert_stmt_list = function
-    [] -> []
-  | [x] -> [convert_stmt x]
-  | _ as l -> (List.map convert_stmt l) *)
-
 let rec get_body_from_body_c = function 
     [] -> []
   | A.Var_dec(A.Local(typ, name, v))::tl -> C.Expr(C.Assign(name, convert_expr v)) :: (get_body_from_body_c tl)
   | _ as x::tl -> (convert_stmt x) :: (get_body_from_body_c tl)
-
 
 let rec get_local_from_body_c = function 
     [] -> []
   | A.Var_dec(A.Local(typ, name, v))::tl -> C.Formal(convert_var_type typ, name) :: (get_local_from_body_c tl)
   | _::tl -> get_local_from_body_c tl
 
-
+(* convert the horizental level function list in A to C *)
 let rec convert_func_list_c m = function
     [] -> []
   | A.Func{A.returnType = r; A.name = n; A.args = a; A.body = b} :: tl -> {
@@ -189,45 +160,8 @@ let rec convert_func_list_c m = function
   } :: (convert_func_list_c m tl)
   | _::tl -> convert_func_list_c m tl
 
-let rec changes = function 
-    [] -> []
-  | _ as x :: tl -> A.Func(x) :: (changes tl)
-
-
+(* entry point *)
 let convert stmts = 
   let funcs = createMain stmts in
   let horizen_funcs_m = convert_func_list StringMap.empty "main" [funcs] in
   convert_func_list_c (snd horizen_funcs_m) (fst horizen_funcs_m)
-
-
-
-
-(*
-(* Variable Type *)
-
-
-
-
-let Convert_graph_op = function
-  | A.Right_Link -> C.Right_Link
-  | A.Left_Link -> C.Left_Link
-  | A.Double_Link -> C.Double_Link
-
-let Convert_expr =
-    A.Num_Lit(n) -> C.Num_Lit(n)
-|   A.Null -> C.Null
-|   A.String_Lit -> C.String_Lit
-|   A.Bool_lit -> B.Bool_Lit
-|   A.Node -> B.Node
-|   A.Graph_Link(e1, op, e2, e3) -> C.Graph
-|   Graph_Link of expr * graph_op * expr * expr
-|   Binop of expr * binop * expr
-|   Unop of unop * expr
-|   Id of string
-|   Assign of string * expr
-|   Noexpr
-|   ListP of expr list
-|   DictP of (expr * expr) list
-|   Call of string * expr list 
- let convert src - function
- 	A.binop a -> Binop_Convert a *)
